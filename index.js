e9bc8b1be14dff893257d1ac246195740c684fc7
AminesCodes <aminebensalem@pursuit.org>
2019-11-05 20:52:33 -0500

    required divFunction in index.js

--- index.js
+++ index.js
@@ -4,6 +4,9 @@
   This is a collaboratively made Math module with support for basic math operations.
   Feel free to use it for you basic math needs.
 */
+
+const div = require('./div');
+
 const Math = {
     PI: 3.141592653589793 // Mathematical constant defined as the ratio of a circle's circumference to its diameter.
   }
