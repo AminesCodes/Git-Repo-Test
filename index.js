fe4182604a3aaeac5be2914289b065cf5b5119c7
vonbarown <vonielbrown@pursuit.org>
2019-11-05 20:57:05 -0500

    crash

--- index.js
+++ index.js
@@ -6,7 +6,8 @@
 */
 const sum = ('./sum.js');
 const Math = {
-  PI: 3.141592653589793 // Mathematical constant defined as the ratio of a circle's circumference to its diameter.
+  PI: 3.141592653589793, // Mathematical constant defined as the ratio of a circle's circumference to its diameter.
+  sum: sum
 }
 
 module.exports = Math
