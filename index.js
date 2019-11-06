a7b9b6377bbba9912007da1a47c8767ccce58216
vonbarown <vonielbrown@pursuit.org>
2019-11-05 20:55:35 -0500

    going for the crash

--- index.js
+++ index.js
@@ -4,8 +4,9 @@
   This is a collaboratively made Math module with support for basic math operations.
   Feel free to use it for you basic math needs.
 */
+const sum = ('./sum.js');
 const Math = {
-    PI: 3.141592653589793 // Mathematical constant defined as the ratio of a circle's circumference to its diameter.
-  }
-  
-  module.exports = Math
+  PI: 3.141592653589793 // Mathematical constant defined as the ratio of a circle's circumference to its diameter.
+}
+
+module.exports = Math
